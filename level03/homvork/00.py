2d6b4baa1b915026eb93c217b366f0255be15d77
ilia30 <iliakartozia84@gmail.com>
2024-05-20 18:50:59 +0400

    commit

---
+++ level03/homvork/00.py
@@ -0,0 +1,6 @@
+print("ilia kartozia 12 clis")
+print("mose maqacaria 5 clis")
+print("koba kartozia 38 clis")
+print("marina maqacaria 37 clis")
+ 
+print("1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20")
