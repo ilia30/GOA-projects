72cf7a30ed35658096852a27d13dfab7cc4af98f
ilia30 <iliakartozia84@gmail.com>
2024-05-28 19:52:09 +0400

    commit

---
+++ level05/homvork/00.py
@@ -0,0 +1,7 @@
+name="ilia"
+surname="kartozia"
+height=" height 1.59"
+lives=" lives in georgia"
+eat=" love pizza"
+color=' like black color'
+print(name +  surname +  height +  lives +  eat + color)
