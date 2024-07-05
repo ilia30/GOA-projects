f28ac902fdeca59a42e8afd1e7d89db5be89d262
ilia30 <iliakartozia84@gmail.com>
2024-07-05 20:10:07 +0400

    commit

--- level15/homevok/02.py
+++ level15/homevok/02.py
@@ -1,1 +1,18 @@
-# and და or  
+#თუ and ოპერატორში 1 არის true და მეორეც არის true გამოიტანს: true
+print(True and True)
+#ხოლო თუ ორივე false-ია გამოიტანს: false
+print(False and False)
+#თუ ერთ მხარეს არის false და მეორე მხარეს არის true ან პირიქით მაინც გამოიანს: false
+print(False and True)
+print(True and False)
+
+
+#or operator
+#თუ or ოპერატორში 1 არის true და მეორეც არის true გამოიტანს: true
+print(True or True)
+#ხოლო თუ ორივე false-ია გამოიტანს: false
+print(False or False)
+#თუ ერთ მხარეს არის false და მეორე მხარეს არის true ან პირიქით მაინც გამოიანს: true
+print(False or True)
+print(True or False)
+
