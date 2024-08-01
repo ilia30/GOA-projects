f5dbbb79504fd44b4492432522927059f6ce695f
ilia30 <iliakartozia84@gmail.com>
2024-08-01 19:04:50 +0400

    commit

---
+++ level24/homevork/00.py
@@ -0,0 +1,42 @@
+for i in range(30):
+    print("goa")
+
+for i in range(1,23,3):
+    print(i)
+
+list=[1,2,3,4,5,6,7,8]
+for i in list:
+    print("sus")
+
+for i in range(10):
+    print(str(i) + ' GOA')
+
+
+    
+colors=["red", "yellou", "green", "black"]
+for i in colors:
+    print(colors)
+
+
+amongus=["sus","sus"]
+                     
+for i in amongus:
+    print(amongus)
+
+
+nambers=["1","2","3","4","5","6"]
+for i in nambers:
+    print(i)
+
+asdfghjk=["asdfghjk","asdfghjk","asdfghjk","asdfghjk","asdfghjk","asdfghjk"]
+for i in asdfghjk:
+    print(asdfghjk)
+
+list=[1,2,3,4,5,6,7,8]
+for i in list:
+    print(i)
+#😁😁😁😁😁😁😁😁😁😁
+
+
+
+
